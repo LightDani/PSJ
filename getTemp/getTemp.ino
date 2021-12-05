a1e2002e04ee2b4e5dd770c563e4a6bbd1f52330
lightDani <cahyafaizi@gmail.com>
2021-12-05 22:31:33 +0700

    add gitignore, upload esp32 code"

---
+++ getTemp/getTemp.ino
@@ -0,0 +1,15 @@
+int outputpin= 36;
+//this sets the ground pin to LOW and the input voltage pin to high
+void setup() {
+Serial.begin(9600); 
+}
+
+void loop()       //main loop
+
+{
+int analogValue = analogRead(outputpin);
+float millivolts = (analogValue/2048.0) * 3100; //3300 is the voltage provided by NodeMCU
+float celsius = millivolts/10;
+Serial.println(celsius);
+delay(10000);
+}
